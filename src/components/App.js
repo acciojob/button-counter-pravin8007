93db7409a69646755f60e06c0a5d24a05229af3b
Pravin Patil <pravinshivajipatil37@gmail.com>
2024-04-11 14:33:22 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
-import React from "react";
+import React ,{ useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+    let increment = () => {
+        setCount(count + 1);
+    }
   return (
     <div>
         {/* Do not remove the main div */}
+     <p>Button Click {count} times</p>
+      <button onClick={increment}>Click me</button>
     </div>
   )
 }
